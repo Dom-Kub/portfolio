fbccbee875b1a59e96b4f1cf5d1c31f368ba088b
Dom-Kub <dominik.kubala21@gmail.com>
2023-02-07 15:52:23 +0100

    [fix] repair scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
-import BACKGROUND from "./component/images/darkBackGround.jpg"
-import AppRoutes from "./AppRoutes";
-import {Parallax, ParallaxLayer} from "@react-spring/parallax"
+import BACKGROUND from './component/images/darkBackGround.jpg';
+import { IParallax, Parallax, ParallaxLayer } from '@react-spring/parallax';
 import AboutMe from './component/Pages/AboutMe';
 import Home from './component/Pages/Home';
 import Experience from './component/Pages/Experience';
 
 function App() {
+  const parallax = useRef<IParallax>(null!);
+
   return (
-    <div>
-      <Parallax pages={2}>
+    <div style={{ width: '100%', height: '100%' }}>
+      <Parallax pages={2} ref={parallax}>
         <ParallaxLayer
-        offset={0}
-        speed={1}
-        factor={2}
-        style={{
-          backgroundImage:`url(${BACKGROUND})`,
-          backgroundSize:'cover',
-          width: '100vw',
-          height: '100vh',
-        }}
+        
+          offset={0}
+          speed={-0.3}
+          factor={2}
+          style={{
+            pointerEvents: 'none',
+            backgroundImage: `url(${BACKGROUND})`,
+            backgroundSize: 'cover',
+            width: '100vw',
+            height: '100vh'
+          }}
         />
         <ParallaxLayer speed={2} offset={0.1}>
-        <Home/>
+          <Home />
         </ParallaxLayer>
-        <ParallaxLayer offset={0.99999999} speed={1} style={{
-          backgroundColor:'black',
-        }}>
-        <AboutMe/>
-          </ParallaxLayer>
-          <ParallaxLayer offset={1} speed={1} style={{
-          backgroundColor:'black',
-          backgroundSize:'cover',
-        }}>
-        <Experience/>
-          </ParallaxLayer>
+        <ParallaxLayer
+          offset={0.99999999}
+          speed={1}
+          style={{
+            backgroundColor: 'black'
+          }}
+        >
+          <AboutMe />
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={1}
+          speed={0.9}
+          style={{
+            backgroundColor: 'black',
+            backgroundSize: 'cover'
+          }}
+        >
+          <Experience />
+        </ParallaxLayer>
       </Parallax>
-   
     </div>
   );
 }
